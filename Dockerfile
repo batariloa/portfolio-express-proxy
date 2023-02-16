ee23d709c267fbd98bf1b59cc2545a6174a304e0
batariloa <batariloa@icloud.com>
2023-02-16 01:08:41 +0100

    gitignore

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY package.json ./
 COPY package-lock.json ./
 COPY ./ ./
 RUN npm i
-
+# Set the PORT environment variable to the value of the Heroku $PORT variable
+ENV PORT $PORT
 EXPOSE 3200
+EXPOSE $PORT
 CMD ["npm", "run", "start"]
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 require("dotenv").config();
+var cors = require("cors");
+
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 console.log("Starting server...");
 
 const app = express();
+
+const corsOptions = {
+  origin: "https://portfolio-ab.herokuapp.com",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 const API_ENDPOINT = "https://api.github.com/users/batariloa/repos?per_page=70";
 const API_KEY = process.env.GITHUB_KEY;
@@ -21,6 +30,6 @@ app.use(
   })
 );
 
-app.listen(3200, () => {
-  console.log("Proxy server listening on port 3200");
+app.listen(process.env.PORT, () => {
+  console.log("Proxy server listening on port ", process.env.PORT);
 });
